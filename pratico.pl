c2b9a16cc9092b4a102dc8f2617267c14903a474
andrepinto42 <andrepinto.goncalves@gmail.com>
2021-11-22 23:02:01 +0000

    Update na query1 para ser mais facil de se ler

--- pratico.pl
+++ pratico.pl
@@ -21,15 +21,15 @@ veiculo(carro,3,100,25).
 % estafeta | cliente | Rating | Distancia | Peso | Veiculo | Encomenda | data do pedido | data de entrega
 entrega(joao,paulo,5,20,15,mota,pao,'25 abril','3 dias').
 entrega(joao,paulo,5,20,15,mota,pc, '10 janeiro', '2 fevereiro').
-entrega(joao,paulo,5,20,15,mota,fones, '').
+entrega(joao,paulo,5,20,15,mota,fones).
 entrega(joao,paulo,5,20,15,bicicleta,fones).
 entrega(pinto,toze,5,20,15,bicicleta,pc).
 entrega(pinto,toze,5,20,15,bicicleta,fones).
 entrega(pinto,toze,5,20,15,bicicleta,pao).
-entrega(daniela,toze,5,20,15,carro,pc).
+% entrega(daniela,toze,5,20,15,carro,pc).
 
 
-entrega(joao,paulo,pao,'26 abril').
+% entrega(joao,paulo,pao,'26 abril').
 %mota = 2 bicicleta =1 carro = 3
 %Preco = PrecoEncomenda + Veiculo * Distancia + (DataFim -DataInicio).
 
@@ -49,50 +49,41 @@ listaCliente(Estafeta,List) :- setof(X,qualCliente(Estafeta,X),List).
 
 
 
+%-----------------------------------------------------
+%-----------------------------------------------------
+%               Query1
+%-----------------------------------------------------
+%-----------------------------------------------------
 
-%Query1
+mostEco(Estafeta) :- findall(X,estafeta(X),[H | T]),  mostEcoAux(T,H,Estafeta).
 
+mostEcoAux( [] , Final ).
+mostEcoAux( [X|Xs] , Final) :-
+  mostEcoAux(Xs,X,Final).
 
-% myRec/2
-myRec( [] , Final ).
-myRec( [X|Xs] , Final) :-
-  myRec(Xs,X,Final).
-
-% myRec/3 (worker predicate)
-myRec( [], Final, Final ).
-myRec( [Estafeta1|Xs] , CurrentEstafeta , Final ) :-
+mostEcoAux( [], Final, Final ).
+mostEcoAux( [Estafeta1|Xs] , CurrentEstafeta , Final ) :-
   compareEstafetaEcologic(Estafeta1,CurrentEstafeta,NewEstafeta),
-  myRec( Xs , NewEstafeta , Final ).
+  write('Current = '),write(CurrentEstafeta),write(' now is '),writeln( NewEstafeta),
+  mostEcoAux( Xs , NewEstafeta , Final ).
 
 
-mostEco(Estafeta) :- showEstafetas([H | T]), ecologicRec(T,H,Estafeta).
-
-test([H|T],Estafeta1,Final) :- compareEstafetaEcologic(H,Estafeta1,Final).
-
-ecologicRec([],_,EstafetaFinal) :- writeln(EstafetaFinal).
-ecologicRec([H|T],Estafeta,EstafetaFinal) :- 
-                                    compareEstafetaEcologic(Head,Estafeta,EstafetaFinal1),
-                                    ecologicRec(T,EstafetaFinal1,EstafetaFinal).
-
-showEstafetas(List) :- findall(X,estafeta(X),List).
-
-
-getAllEstafetas(List) :- findall(Y,estafeta(Y),List).
-
-ecologicoMain(Estafeta,Nivel) :- findall(X,ecologicoEstafeta(Estafeta,X),List),
+ecologicoNivel(Estafeta,Nivel) :- findall(X,ecologicoEstafeta(Estafeta,X),List),
                                 sumlist(List,Tamanho),
                                 length(List,TamLista),
+                                (TamLista == 0 -> TamLista is -1;true),
                                 Nivel is Tamanho / TamLista.
 
-ecologicoEstafeta(Estafeta,Rating) :- entrega(Estafeta,_,_,_,_,Veiculo,_),
-                                    veiculo(Veiculo,Rating,_,_).
-
 compareEstafetaEcologic(Estafeta1,Estafeta2,EstafetaFinal) :- 
-                                                            ecologicoMain(Estafeta1,Nivel1),
-                                                            ecologicoMain(Estafeta2,Nivel2),
+                                                            ecologicoNivel(Estafeta1,Nivel1),
+                                                            ecologicoNivel(Estafeta2,Nivel2),
+                                write('Nivel = '),write(Nivel1),write(' para '),writeln(Estafeta1),
+                                write('Nivel = '),write(Nivel2),write(' para '),writeln(Estafeta2),
+                                                            (Nivel2 =< 0 -> EstafetaFinal = Estafeta1;true),
+                                                            (Nivel1 =< 0 -> EstafetaFinal = Estafeta2;true),
                                                             Nivel1>Nivel2 ->EstafetaFinal = Estafeta2;
                                                             EstafetaFinal = Estafeta1.
 
-
-
+ecologicoEstafeta(Estafeta,Rating) :- entrega(Estafeta,_,_,_,_,Veiculo,_),
+                                    veiculo(Veiculo,Rating,_,_).
                             
